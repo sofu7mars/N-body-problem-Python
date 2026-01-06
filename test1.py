61971b80fea9c83882ac6a440f52c154f663491d
sofu7mars <vko300748@gmail.com>
2026-01-06 10:02:58 +0100

    w

--- test1.py
+++ test1.py
@@ -6,14 +6,14 @@ if __name__ == '__main__':
 
     universe = Universe(1, 10, 0.001)
 
-    universe.create_N_bodies_with_random_pos_vel(n_bodies = 4, masses = [1.7, 5, 2.5, 2.1])
+    universe.create_N_bodies_with_random_pos_vel(n_bodies = 7, masses = [0.5, 0.7, 1, 0.9, 0.63, 0.6, 0.8])
 
 
-#     universe.solve()
+    universe.solve()
     universe.animate3d(
-            from_file = True,
+            from_file = False,
             trace_length_dev = 25,
             padding_factor = 1,
-            track_body_index = 1,
+            track_body_index = 2,
             animation_step = 1
     )
--- test2_solar_system.py
+++ test2_solar_system.py
@@ -15,10 +15,18 @@ if __name__ == '__main__':
         velocity = [0, 24100, 0]
     ))
 
+    universe.add_body(Body(
+        name = 'Comet',
+        mass = 6e10,
+        position = [0, 2.28e11, 0],
+        velocity = [0, 0, 53100]
+    ))
+
+
     for b in universe.bodies:
         print(b.name)
 
-    universe.solve()
-    universe.plot2d(from_file = False, axises = 'xy', body_index = 1)
-    universe.animate3d(from_file = True, trace_lines = True, fading_trace_lines = True, trace_length_dev = 25, padding_factor = 1,
-                    track_body_index = 2, animation_step = 1000)
+    #universe.solve()
+    #universe.plot2d(from_file = False, axises = 'xy', body_index = 1)
+    universe.animate3d(from_file = True, trace_lines = True, fading_trace_lines = True, trace_length_dev = 75, padding_factor = 1,
+                    track_body_index = None, animation_step = 1000)
